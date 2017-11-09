0e4e60783c25d2780b3cf0f7fa8c96de19af7607
Gilbertat <shiyue45457@gmail.com>
2017-11-09 18:54:19 +0800

    User can create topic

--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,1 +1,7 @@
-左边导航栏
+<div class="panel panel-default">
+    <div class="panel-body">
+        <a href="{{ route('topics.create') }}" class="btn btn-success btn-block" aria-hidden="true">
+            <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>新建帖子
+        </a>
+    </div>
+</div>
