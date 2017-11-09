1a306501033138b0b1ed47368bb5f44403f52854
Gilbertat <shiyue45457@gmail.com>
2017-11-09 19:06:46 +0800

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href=" {{ asset('css/app.css') }}">
+        @yield('styles')
     </head>
     <body>
         <div id="app" class="{{ route_class() }}-page">
@@ -21,6 +22,8 @@
             </div>
             @include('layouts._footer')
         </div>
+        <!-- Scripts -->
         <script src=" {{ asset('js/app.js') }}"></script>
+        @yield('scripts')
     </body>
 </html>
