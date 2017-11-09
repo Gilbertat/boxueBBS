0b3f3dc6bb3a6c4dc739b9a8b97dc4e182da24b7
Gilbertat <shiyue45457@gmail.com>
2017-11-09 16:55:06 +0800

    Topic index page

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,7 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+        $topics = Topic::query()->with('user', 'category')->paginate(30);
 		return view('topics.index', compact('topics'));
 	}
 
