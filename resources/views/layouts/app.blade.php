718b7d244559d35567b6945cacd9cd96cac8deac
Gilbertat <shiyue45457@gmail.com>
2017-11-15 11:17:27 +0800

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <!-- CSRF Token-->
         <meta name="csrf-token" content=" {{ csrf_token() }}">
         <title>@yield('title', 'boxueBBS') - 泊学论坛</title>
-
+        <meta name="description" content="@yield('description', '泊学论坛')" />
         <!-- Styles -->
         <link rel="stylesheet" href=" {{ asset('css/app.css') }}">
         @yield('styles')
