5c1c6287c3a6cb692baaf789b2b28f9a659b3173
Gilbertat <shiyue45457@gmail.com>
2017-11-17 00:58:08 +0800

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,7 +2,7 @@
 
 namespace App\Observers;
 
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -24,11 +24,18 @@ class TopicObserver
     {
         // XSS 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
+
         // 生成摘要
         $topic->excerpt = make_excerpt($topic->body);
+
+    }
+
+    public function saved(Topic $topic)
+    {
         // 中文转英文
         if (! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // 推送任务队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
